677c4ee084d2db532b8939aa843fffba230ce852
manok119 <manok1109@gmail.com>
2022-03-04 14:40:37 +0900

    api

--- routes/items/read.js
+++ routes/items/read.js
@@ -1,10 +1,43 @@
 'use strict'
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+module.exports = async function (app, opts) {
+  app.get('/', async function (request, reply) {
+        const result = await readAll(this.mongo)
+    
+        reply
+          .code(200)
+          .header('Content-Type', 'application/json; charset=utf-8')
+         .send({
+          pid: 1234,
+          name: "item1",
+          price: 3000
+          },
+          {
+          pid: 1235,
+          name: item2,
+          price: 4000
+          },
+          {
+          pid: 1236,
+          name: "item3",
+          price: 5000
+          })
+      })
+    
+      app.get('/:id', async function (request, reply) {
+       const result = await readOne(this.mongo, request.params.id)
+    
+       reply
+          .code(200)
+         .header('Content-Type', 'application/json; charset=utf-8')
+         .send({
+          id: 1234,
+          name: "item1",
+          price: 3000
+          })
+      })
+    }
+    
 
 
 //'use strict'
--- routes/users/create.js
+++ routes/users/create.js
@@ -1,28 +1,14 @@
 'use strict'
 
-module.exports = async function (fastify, opts) {
-  fastify.post('/', async function (request, reply) {
-    return 'this is an example'
+module.exports = async function (app, opts) {
+  app.post('/', async function (request, reply) {
+    //const result = await createOne(this.mongo, request.body)
+
+    reply
+      .code(201)
+      .header('content-type', 'application/json')
+      .send({
+        user_id: "abcd"
+      })
   })
 }
-
-
-
-// //
-// 'use strict'
-
-// const { createOne } = require('../../model')
-
-// module.exports = async function (app, opts) {
-//   app.post('/', async function (request, reply) {
-//     const result = await createOne(this.mongo, request.body)
-
-//     reply
-//       .code(201)
-//       .header('Content-Type', 'application/json; charset=utf-8')
-//       .send({
-//         id: result.insertedId.toString()
-//       })
-//   })
-// }
-
