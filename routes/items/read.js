eb55fc01280d33b95b4ef7c3816ac0bc432e0fe8
seungyeoniii <alpcarmy@gmail.com>
2022-03-04 16:42:18 +0900

    model added sylee

--- routes/items/read.js
+++ routes/items/read.js
@@ -2,41 +2,14 @@
 
 module.exports = async function (app, opts) {
   app.get('/', async function (request, reply) {
-    //const result = await readAll(this.mongo)
+    const result = await readAll(this.mongo)
     
     reply
       .code(200)
       .header('Content-Type', 'application/json; charset=utf-8')
-      .send([{
-        pid: 1234,
-        name: "item1",
-        price: 3000
-        },
-        {
-        pid: 1235,
-        name: "item2",
-        price: 4000
-        },
-        {
-        pid: 1236,
-        name: "item3",
-        price: 5000
-        }])
+      .send(result)
       })
-    
-  app.get('/:id', async function (request, reply) {
-    //const result = await readOne(this.mongo, request.params.id)
-    
-       reply
-          .code(200)
-         .header('Content-Type', 'application/json; charset=utf-8')
-         .send({
-          pid: 1234,
-          name: "item1",
-          price: 3000
-          })
-      })
-    }
+}
     
 
 
